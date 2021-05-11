71943db15826ba84cfc9648cd79f3dca1943aaa0
dongchan-K <ehdcks1028@gmail.com>
2021-05-11 15:39:44 +0900

    feat: 봉우리 문제풀이 추가

--- 인프런 알고리즘/1, 2차원 배열 탐색/봉우리.js
+++ 인프런 알고리즘/1, 2차원 배열 탐색/봉우리.js
@@ -1,4 +1,34 @@
 function solution(arr) {
+  let answer = 0;
+  // 좌우, 상하 비교를 위한 변수 dx, dy
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      // 봉우리인지 확인하기 위한 변수
+      let check = true;
+      // dx, dy의 모든 값을 비교하기 위한 반복문
+      for (let k = 0; k < dx.length; k++) {
+        let nx = i + dx[k];
+        let ny = j + dy[k];
+        if (
+          nx >= 0 &&
+          nx < arr.length &&
+          ny >= 0 &&
+          ny < arr.length &&
+          arr[nx][ny] >= arr[i][j]
+        ) {
+          check = false;
+          break;
+        }
+      }
+      if (check) {
+        answer++;
+      }
+    }
+  }
+
   return answer;
 }
 
