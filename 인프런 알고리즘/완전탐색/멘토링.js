e63355aeeeda333ecdedbcd8fbc82e228577eec1
dongchan-K <ehdcks1028@gmail.com>
2021-05-13 23:43:45 +0900

    docs: 완전탐색 디렉토리 및 문제 추가

---
+++ 인프런 알고리즘/완전탐색/멘토링.js
@@ -0,0 +1,10 @@
+function solution(test) {
+  return answer;
+}
+
+const arr = [
+  [3, 4, 1, 2],
+  [4, 3, 2, 1],
+  [3, 1, 4, 2],
+];
+console.log(solution(arr));
---
+++ 인프런 알고리즘/완전탐색/자리수의 합.js
@@ -0,0 +1,6 @@
+function solution(n, arr) {
+  return answer;
+}
+
+const arr = [128, 460, 603, 40, 521, 137, 123];
+console.log(solution(7, arr));
