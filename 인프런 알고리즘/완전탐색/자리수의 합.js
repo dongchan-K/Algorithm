2f06f372e784ce7108691298d97f53dd501c79f0
dongchan-K <ehdcks1028@gmail.com>
2021-05-16 21:19:56 +0900

    feat: 자리수의 합 문제풀이 추가

--- 인프런 알고리즘/완전탐색/자리수의 합.js
+++ 인프런 알고리즘/완전탐색/자리수의 합.js
@@ -1,4 +1,23 @@
 function solution(n, arr) {
+  let answer;
+  let currValue = Number.MIN_SAFE_INTEGER;
+  let maxValue = 0;
+
+  for (let i = 0; i < n; i++) {
+    const str = arr[i] + '';
+    let sum = 0;
+    for (let j = 0; j < str.length; j++) {
+      sum += parseInt(str[j]);
+    }
+    if (sum > currValue) {
+      currValue = sum;
+      maxValue = arr[i];
+    } else if (sum === currValue) {
+      maxValue = arr[i] > maxValue ? arr[i] : maxValue;
+    }
+    answer = maxValue;
+  }
+
   return answer;
 }
 
