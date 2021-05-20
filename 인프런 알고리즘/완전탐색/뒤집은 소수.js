9a623cdcaf6429eb54094da9e6de02ebfcaf96f9
dongchan-K <ehdcks1028@gmail.com>
2021-05-21 00:57:23 +0900

    feat: 뒤집은 소수 문제풀이 추가

--- 인프런 알고리즘/완전탐색/뒤집은 소수.js
+++ 인프런 알고리즘/완전탐색/뒤집은 소수.js
@@ -1,6 +1,21 @@
-function isPrime(num) {
+// 소수 판별 함수
+const isPrime = num => {
+  if (num === 1) return;
+  for (let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const solution = arr => {
+  let answer = [];
+  for (const x of arr) {
+    // 숫자 -> 문자열 -> 뒤집기 -> 10진수 숫자로 변경
+    let res = parseInt(x.toString().split('').reverse().join(''), 10);
+    if (isPrime(res)) answer.push(res);
+  }
   return answer;
-}
+};
 
 const arr = [32, 55, 62, 20, 250, 370, 200, 30, 100];
 console.log(solution(arr));
