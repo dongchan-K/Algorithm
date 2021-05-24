e33ba35fef270408a61a96285a9a29e52e49fbc7
dongchan-K <ehdcks1028@gmail.com>
2021-05-24 11:25:36 +0900

    feat: 멘토링 문제풀이 추가

--- 인프런 알고리즘/완전탐색/멘토링.js
+++ 인프런 알고리즘/완전탐색/멘토링.js
@@ -1,4 +1,25 @@
 function solution(test) {
+  let answer = 0;
+  const testCount = test.length; // 시험 횟수
+  const student = test[0].length; // 학생 수
+
+  // 1번 학생부터 4번 학생까지 멘토, 멘티 경우의 수 4 x 4
+  for (let i = 1; i <= student; i++) {
+    for (let j = 1; j <= student; j++) {
+      let count = 0;
+      for (let k = 0; k < testCount; k++) {
+        let pi = 0; // i 학생 등수
+        let pj = 0; // j 학생 등수
+        for (let s = 0; s < student; s++) {
+          if (test[k][s] === i) pi = s;
+          if (test[k][s] === j) pj = s;
+        }
+        if (pi < pj) count++;
+      }
+      if (count === testCount) answer++;
+    }
+  }
+
   return answer;
 }
 
@@ -7,4 +28,5 @@ const arr = [
   [4, 3, 2, 1],
   [3, 1, 4, 2],
 ];
+
 console.log(solution(arr));
