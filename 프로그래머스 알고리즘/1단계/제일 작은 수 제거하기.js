c71eb9d40fb1c240f9e9c5788d5acca6c28b076e
dongchan-K <ehdcks1028@gmail.com>
2021-05-30 16:59:00 +0900

    feat: 제일 작은 수 제거하기 문제풀이 추가

---
+++ 프로그래머스 알고리즘/1단계/제일 작은 수 제거하기.js
@@ -0,0 +1,7 @@
+function solution(arr) {
+  arr.splice(arr.indexOf(Math.min(...arr)), 1);
+  return arr.length ? arr : [-1];
+}
+
+console.log(solution([4, 3, 2, 1])); // [4, 3, 2]
+console.log(solution([10])); // [-1]
