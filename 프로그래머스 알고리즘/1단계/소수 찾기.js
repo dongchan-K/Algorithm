4ecccb40c9e345d7dc0b8a51205cc296b921043d
dongchan-K <ehdcks1028@gmail.com>
2021-05-30 17:04:58 +0900

    feat: 소수 찾기 문제풀이 추가

---
+++ 프로그래머스 알고리즘/1단계/소수 찾기.js
@@ -0,0 +1,17 @@
+function solution(n) {
+  let arr = Array(n + 1)
+    .fill(true)
+    .fill(false, 0, 2);
+
+  for (let i = 2; i * i <= n; i++) {
+    if (arr[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        arr[j] = false;
+      }
+    }
+  }
+  return arr.filter(num => num).length;
+}
+
+console.log(solution(10)); // 4
+console.log(solution(5)); // 3
